a29c0b147a44a3d98d811123e9de40b48adbf67d
zajacignacy@gmail.com <zajacignacy@gmail.com>
2025-06-12 23:45:26 +0200

    update models

--- backend/app/db/team.py
+++ backend/app/db/team.py
@@ -7,10 +7,10 @@ from .database import Base
 class Team(Base):
     __tablename__ = "teams"
 
-    squad_id = Column(String, ForeignKey("squads.squad_id", ondelete="CASCADE"))
-    match_id = Column(String, ForeignKey("matches.match_id", ondelete="CASCADE"))
+    squad_id = Column(String(36), ForeignKey("squads.squad_id", ondelete="CASCADE"))
+    match_id = Column(String(36), ForeignKey("matches.match_id", ondelete="CASCADE"))
     team_id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    color = Column(String, nullable=False) # white , black or color or maybe really a RGB value
+    color = Column(String(255), nullable=False) # white , black or color or maybe really a RGB value
     name = Column(String(255), nullable=True)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     score = Column(Integer, nullable=False, default=0)
