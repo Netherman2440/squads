61f2c94b2ddd93dec7461931909ce8ac0fc08684
zajacignacy@gmail.com <zajacignacy@gmail.com>
2025-06-15 23:12:20 +0200

    alembic initial migration - create tables

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,20 +4,18 @@ import dotenv
 from fastapi import FastAPI
 from app.routes import auth
 from app.routes import squads
-from backend.app.database import create_tables_if_not_exist
 
 #.\.venv\Scripts\activate
 #pip freeze > requirements.txt
 #pip install -r requirements.txt
 #uvicorn app.main:app --reload  
+#alembic init migrations
+#alembic upgrade head
 app = FastAPI(
     title="Squads API",
     description="API for managing football squads, players, and matches",
     version="1.0.0"
 )
-
-create_tables_if_not_exist()
-
 
 app.include_router(auth.router, prefix="/api/v1")
 #app.include_router(players.router, prefix="/api/v1/")
